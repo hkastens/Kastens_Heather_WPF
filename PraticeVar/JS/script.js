edd64fa2baa6a349337661a6a63ec93e289c73a4
hkastens <hkastens@fullsail.edu>
2013-11-03 23:29:20 -0600

    Pratice Var

---
+++ PraticeVar/JS/script.js
@@ -0,0 +1,25 @@
+// Defining Variables
+
+var age; //declaration
+age = 30; //definition
+
+//storage container = value
+
+var yearOfBirth = 1983; //declaring and defining on the same line 
+
+var isStudent = false; //Boolean Var
+
+var firstName = "Heather";
+
+var phrase = "I don't know";
+
+
+
+
+
+//Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo" ]; //0,1,2,3
+
+muppetNames[1] = "Rizzo";
+console.log(muppetNames[1])
