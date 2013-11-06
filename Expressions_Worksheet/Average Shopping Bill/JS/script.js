0da72028cd564385cc20fc423c5b0955d25454eb
hkastens <hkastens@fullsail.edu>
2013-11-06 00:20:46 -0600

    Total and average found

--- Expressions_Worksheet/Average Shopping Bill/JS/script.js
+++ Expressions_Worksheet/Average Shopping Bill/JS/script.js
@@ -3,7 +3,13 @@
 //Expressions Worksheet
 //Average Shopping Bill
 
-var weeklyBudget [206, 220, 230,250,219];
+var weeklyBudget = [206, 220, 230, 250, 219];
 
+var total = weeklyBudget[0] + weeklyBudget[1] + weeklyBudget[2] + weeklyBudget[3] + weeklyBudget[4];
 
+console.log("My total amount spent was " + total);
+
+var average = total/5; 
+
+console.log("My average amount spent spent was " + average);
 
