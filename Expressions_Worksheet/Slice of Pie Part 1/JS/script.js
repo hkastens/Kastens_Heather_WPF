7173be77b18c098bf57540ae65703771a73fe13e
hkastens <hkastens@fullsail.edu>
2013-11-05 23:12:56 -0600

    JS file created and vars identified.

---
+++ Expressions_Worksheet/Slice of Pie Part 1/JS/script.js
@@ -0,0 +1,10 @@
+//Heather Kastens
+//November 5, 2013
+//Expressions Worksheet
+//Slice of Pie Part 1
+
+var numberOfPizzas = 6;
+var numberOfPeople = 12;
+var numberOfSlices = 8;
+
+var numberOfSlicesPerPerson = 
