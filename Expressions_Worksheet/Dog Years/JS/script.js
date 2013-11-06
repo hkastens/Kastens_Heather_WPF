0fffd587f2c7cc64aa6bd650a940ff68b035c6f6
hkastens <hkastens@fullsail.edu>
2013-11-05 23:00:09 -0600

    Comments added.

--- Expressions_Worksheet/Dog Years/JS/script.js
+++ Expressions_Worksheet/Dog Years/JS/script.js
@@ -3,10 +3,10 @@
 //Expression Worksheet
 //Dog Years
 
-var humanYears = 1;
+var humanYears = 1; //The age Sparky is in human years // Is changeable
 
-const dogYears = 7;
+const dogYears = 7; //This number will stay the same //For every 1 human year it 7 years in dog years 
 
-var sparkysDogYearsAge = humanYears * dogYears;
+var sparkysDogYearsAge = humanYears * dogYears; //To find out the total amount of dogs years sparky is you take his human years and multiply it ny 7
 
-console.log(" Sparkys age in dog years is " + sparkysDogYearsAge);
+console.log(" Sparkys age in dog years is " + sparkysDogYearsAge); //This is how old sparky is in dog years. 
