727af057122d809c0b8b8e059a3eeae02a58eb67
hkastens <hkastens@fullsail.edu>
2013-11-05 22:54:58 -0600

    JS file was written. Includes equation with const and var.

---
+++ Expressions_Worksheet/Dog Years/JS/script.js
@@ -0,0 +1,12 @@
+//Heather Kastens
+//November 5, 2013
+//Expression Worksheet
+//Dog Years
+
+var humanYears = 1;
+
+const dogYears = 7;
+
+var sparkysDogYearsAge = humanYears * dogYears;
+
+console.log(" Sparkys age in dog years is " + sparkysDogYearsAge);
