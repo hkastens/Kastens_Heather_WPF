27b8b5ff31de7d4aef36af34e41571ca44b0e9f6
hkastens <hkastens@fullsail.edu>
2013-11-05 23:22:58 -0600

    Comments added

--- Expressions_Worksheet/Slice of Pie Part 1/JS/script.js
+++ Expressions_Worksheet/Slice of Pie Part 1/JS/script.js
@@ -3,10 +3,10 @@
 //Expressions Worksheet
 //Slice of Pie Part 1
 
-var numberOfPizzas = 6;
-var numberOfPeople = 12;
-var numberOfSlices = 8;
+var numberOfPizzas = 6; //This is the number of pizzas ordered
+var numberOfPeople = 12; //This is the number of people attending
+var numberOfSlices = 8; //The number of slices per a pizza
 
-var numberOfSlicesPerPerson = numberOfPizzas * numberOfSlices / numberOfPeople;
+var numberOfSlicesPerPerson = numberOfPizzas * numberOfSlices / numberOfPeople; //To find total amount of slices per a person you take the number of pizzas multiply the number of slices and then divide by the total amount of people
 
-console.log("The total number of pizza slices per a person is " + numberOfSlicesPerPerson); 
+console.log("The total number of pizza slices per a person is " + numberOfSlicesPerPerson); //Total number of slices per a person printed to console. 
