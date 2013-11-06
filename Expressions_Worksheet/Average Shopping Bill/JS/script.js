f0054319e599b3067cebd4c455f577f729470159
hkastens <hkastens@fullsail.edu>
2013-11-06 00:22:44 -0600

    Comments added

--- Expressions_Worksheet/Average Shopping Bill/JS/script.js
+++ Expressions_Worksheet/Average Shopping Bill/JS/script.js
@@ -3,13 +3,13 @@
 //Expressions Worksheet
 //Average Shopping Bill
 
-var weeklyBudget = [206, 220, 230, 250, 219];
+var weeklyBudget = [206, 220, 230, 250, 219]; //My array
 
-var total = weeklyBudget[0] + weeklyBudget[1] + weeklyBudget[2] + weeklyBudget[3] + weeklyBudget[4];
+var total = weeklyBudget[0] + weeklyBudget[1] + weeklyBudget[2] + weeklyBudget[3] + weeklyBudget[4]; //To find total simply add each number together
 
-console.log("My total amount spent was " + total);
+console.log("My total amount spent was " + total); //total printed to console
 
-var average = total/5; 
+var average = total/5; //To find average you take the total and divide by the number of amounts that are listed
 
-console.log("My average amount spent spent was " + average);
+console.log("My average amount spent spent was " + average); //average printed to console
 
