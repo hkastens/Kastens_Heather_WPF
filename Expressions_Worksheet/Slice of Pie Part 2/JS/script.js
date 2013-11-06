80341c5541d85683764ead4ea82db4ec3d0677b7
hkastens <hkastens@fullsail.edu>
2013-11-05 23:46:13 -0600

    Equation tested and printed to console.

--- Expressions_Worksheet/Slice of Pie Part 2/JS/script.js
+++ Expressions_Worksheet/Slice of Pie Part 2/JS/script.js
@@ -6,3 +6,7 @@
 var numberOfPeople = 11;
 var numberOfPizzas = 6;
 var numberOfSlices = 8;
+
+var numberOfSlicesSparkyGets = numberOfPizzas * numberOfSlices % numberOfPeople;
+ 
+console.log("The number of pizzas that Sparky get will be " + numberOfSlicesSparkyGets);
