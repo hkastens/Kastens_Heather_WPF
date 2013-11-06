1466b948b8f6bf3e6334618b961bc01325de1620
hkastens <hkastens@fullsail.edu>
2013-11-05 23:50:16 -0600

    Comments added

--- Expressions_Worksheet/Slice of Pie Part 2/JS/script.js
+++ Expressions_Worksheet/Slice of Pie Part 2/JS/script.js
@@ -3,10 +3,10 @@
 //Expression Worksheet
 //Slice of Pie Part 2
 
-var numberOfPeople = 11;
-var numberOfPizzas = 6;
-var numberOfSlices = 8;
+var numberOfPeople = 11; //The number of people at party
+var numberOfPizzas = 6; //The number of pizzas ordered
+var numberOfSlices = 8; //The number of slices per pizza 
 
-var numberOfSlicesSparkyGets = numberOfPizzas * numberOfSlices % numberOfPeople;
+var numberOfSlicesSparkyGets = numberOfPizzas * numberOfSlices % numberOfPeople; //To find the slices of pizza that Sparky gets you take the total number of pizzas and multiple it by the number of slices. Then instead of using the / sign you use the % sign or the modulo operator to find the remainder. 
  
-console.log("The number of pizzas that Sparky get will be " + numberOfSlicesSparkyGets);
+console.log("The number of pizzas that Sparky gets will be " + numberOfSlicesSparkyGets); //Thios is the amount of slices that Sparky gets.
