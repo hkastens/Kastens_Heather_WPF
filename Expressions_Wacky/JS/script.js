f9c11b8fd13bb023c29fe37bf4d0e4e4a97fb309
hkastens <hkastens@fullsail.edu>
2013-11-07 14:49:28 -0600

    Prompt 3 and 4 created

--- Expressions_Wacky/JS/script.js
+++ Expressions_Wacky/JS/script.js
@@ -4,4 +4,8 @@
 
 var Opening = prompt ("I'm having a party and need to know how many \ntables I need");
 var numberPeople = prompt ("Please enetr how many people will be attending");
+var table = prompt ("Each table sits 6");
+
+var totalTables = numberPeople /6;
+
 
