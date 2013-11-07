c90a3dba48a2c8ee7684dcbd9af58a6ffb868014
hkastens <hkastens@fullsail.edu>
2013-11-07 14:26:00 -0600

    Was having to hard of a problem getting my calcultor to work. started over

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -2,12 +2,3 @@
 //November 7, 2013
 //Expressions_Industry
 
-var amount1 = prompt ("We are trying to find the average \namount of chicken made daily. \nPlease enter the amounts in the following prompts.\nAmount of chicken made in first round.");
-var amount2 = prompt ("Amount made in second round");
-var amount3 = prompt ("Amount made in third round");
-var amount4 = prompt ("Amount made in forth round");
-
-var average = amount1 + amount2+ amount3 + amount4 / 4;
-
-var results = ("The average amount of chicken dropped for today was " + average );
-alert(results);
