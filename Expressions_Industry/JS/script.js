bf51f7f665179698801d5017ab98779cdd3123ed
hkastens <hkastens@fullsail.edu>
2013-11-07 14:33:27 -0600

    Calculator created and tested.

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -4,5 +4,7 @@
 
 var hrsWorked = prompt ("How many hours did you work today?");
 var hrWage = prompt ("How much do you make an hour?");
+var totalAmountMade = hrsWorked * hrWage; 
 
+alert("The total amount of money that you made today is $" +totalAmountMade)
 
