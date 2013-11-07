f69b38ba3381458b4e589c279c09d09ae7d51a20
hkastens <hkastens@fullsail.edu>
2013-11-07 14:30:19 -0600

    Edited prompt 2

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -2,5 +2,7 @@
 //November 7, 2013
 //Expressions_Industry
 
-var hrsWorked = prompt ("How many hours did you work today?")
-var daysWorked = prompt ("How many days this week did you work?")
+var hrsWorked = prompt ("How many hours did you work today?");
+var hrWage = prompt ("How much do you make an hour?");
+
+
