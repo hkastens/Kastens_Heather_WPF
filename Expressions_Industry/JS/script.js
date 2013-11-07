0556e383051d24fe6460049742a495387882902f
hkastens <hkastens@fullsail.edu>
2013-11-07 13:26:54 -0600

    My calculator did not work. Had to delete

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -6,3 +6,5 @@ var amount1 = prompt ("We are trying to find the average \namount of chicken mad
 var amount2 = prompt ("Amount made in second round");
 var amount3 = prompt ("Amount made in third round");
 var amount4 = prompt ("Amount made in forth round");
+
+
