fa01c40403cb89c538283da779b291f73f31cc5c
hkastens <hkastens@fullsail.edu>
2013-11-07 15:01:09 -0600

    comments added.

--- Expressions_Wacky/JS/script.js
+++ Expressions_Wacky/JS/script.js
@@ -2,11 +2,12 @@
 //November 7, 2013
 //Expressions Wacky
 
-alert("Your having a party and I need to know how many \ntables I need to bring");
-var numberPeople = prompt ("Please enter how many people will be attending");
-alert("Each table sits 6");
+//I'm a party host and need to how how much setting is needed. 
+alert("You're having a party and I need to know how many \ntables I need to bring"); //explaining to customer what is about to be asked
+var numberPeople = prompt ("Please enter how many people will be attending");// asking how many guests they will have
+alert("Each table sits 6"); //explaining that each table sits 6 people
 
-var totalTables = numberPeople /6;
-alert("So the total numbers of tables that I need to bring is " + totalTables);
+var totalTables = numberPeople /6; // to find the number of tables need to take the amont of people divide by 6 which is the number of seats at each table
+alert("So the total numbers of tables that I need to bring is " + totalTables);// this shows the customer how many tables i'll be bringing
 
 
