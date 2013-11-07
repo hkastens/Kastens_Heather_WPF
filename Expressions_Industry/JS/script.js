1be12279c1da97ea17c6907ddf1abe1b4294ab6f
hkastens <hkastens@fullsail.edu>
2013-11-07 14:35:49 -0600

    comments

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -2,9 +2,9 @@
 //November 7, 2013
 //Expressions_Industry
 
-var hrsWorked = prompt ("How many hours did you work today?");
-var hrWage = prompt ("How much do you make an hour?");
-var totalAmountMade = hrsWorked * hrWage; 
+var hrsWorked = prompt ("How many hours did you work today?"); //The number of hours worked
+var hrWage = prompt ("How much do you make an hour?"); //The amount made per an hour
+var totalAmountMade = hrsWorked * hrWage; //Take the number of hours times the amount you make an hour
 
-alert("The total amount of money that you made today is $" +totalAmountMade)
+alert("The total amount of money that you made today is $" +totalAmountMade) //Shows how much money you day for the day
 
