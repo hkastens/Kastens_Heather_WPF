a39b1e9003154613e0258ea4525db3d0f36ceb4c
hkastens <hkastens@fullsail.edu>
2013-11-07 14:47:52 -0600

    Prompt 1 and 2 created

--- Expressions_Wacky/JS/script.js
+++ Expressions_Wacky/JS/script.js
@@ -1,3 +1,7 @@
 //Heather Kastens
 //November 7, 2013
 //Expressions Wacky
+
+var Opening = prompt ("I'm having a party and need to know how many \ntables I need");
+var numberPeople = prompt ("Please enetr how many people will be attending");
+
