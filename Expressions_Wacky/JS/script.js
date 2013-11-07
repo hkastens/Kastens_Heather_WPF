0c9b250e96511b988a8ce8a3211f877b16b32e04
hkastens <hkastens@fullsail.edu>
2013-11-07 14:51:52 -0600

    Changed first prompt to an alert as I do not need any information inputted there.

--- Expressions_Wacky/JS/script.js
+++ Expressions_Wacky/JS/script.js
@@ -2,10 +2,11 @@
 //November 7, 2013
 //Expressions Wacky
 
-var Opening = prompt ("I'm having a party and need to know how many \ntables I need");
+alert("I'm having a party and need to know how many \ntables I need");
 var numberPeople = prompt ("Please enetr how many people will be attending");
 var table = prompt ("Each table sits 6");
 
 var totalTables = numberPeople /6;
+alert(totalTables);
 
 
