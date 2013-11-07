40fc9b67ac5a29080e32e6f595db00654f7c09bb
hkastens <hkastens@fullsail.edu>
2013-11-07 13:04:31 -0600

    Comments added

--- Expressions_Personal/JS/script.js
+++ Expressions_Personal/JS/script.js
@@ -2,12 +2,12 @@
 //November 7, 2013
 //Expressions_Personal
 
-var paycheck =prompt ("The total amount of paycheck");
-var amountTakenOut = prompt ("The amount that you wish to put back in savings");
+var paycheck =prompt ("The total amount of paycheck"); //Your total amount that your paycheck was
+var amountTakenOut = prompt ("The amount that you wish to put back in savings"); //how much you want to set aside for savings
 
-var thePercentage = amountTakenOut / paycheck;
+var thePercentage = amountTakenOut / paycheck; //the equation to find the percentage of what you took out
 
-var results = "The percentage taken out of your paycheck for saving is " + thePercentage; 
+var results = "The percentage taken out of your paycheck for saving is " + thePercentage; //The alert that tells you how much you put back
 
-alert(results);
+alert(results);//Made the last alert appear
 
