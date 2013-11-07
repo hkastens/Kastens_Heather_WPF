94071e201c344fbd631f414f493694e5eeb0f334
hkastens <hkastens@fullsail.edu>
2013-11-07 13:17:36 -0600

    Prompts 1-4 created.

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -2,3 +2,7 @@
 //November 7, 2013
 //Expressions_Industry
 
+var amount1 = prompt ("We are trying to find the average \namount of chicken made daily. \nPlease enter the amounts in the following prompts.\nAmount of chicken made in first round.");
+var amount2 = prompt ("Amount made in second round");
+var amount3 = prompt ("Amount made in third round");
+var amount4 = prompt ("Amount made in forth round");
