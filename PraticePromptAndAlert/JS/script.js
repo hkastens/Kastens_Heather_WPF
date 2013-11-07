7b5bd386695a0b9e4aeaa615ef0592c32edb0c18
hkastens <hkastens@fullsail.edu>
2013-11-07 12:16:54 -0600

    JS created and script written

---
+++ PraticePromptAndAlert/JS/script.js
@@ -0,0 +1,13 @@
+//Pratice Prompt and Alert
+
+var width = prompt ("We are calclating the are of a rectangle. \nPlease enter weidth");
+var height = prompt ("Please enter the height");
+var area = width * height; //calculates with that info
+
+//console.log(area); //prints to console
+
+var result = " The area of your rectangle is " + area + "sq feet";
+alert(result);
+
+
+
