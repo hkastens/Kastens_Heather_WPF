33d93b0f930e51886f6528142ba9c52fbdb1f480
hkastens <hkastens@fullsail.edu>
2013-11-13 16:48:00 -0600

    Prac Basic Conditionals

---
+++ PraticeReadingResources/PracBasicConditional/JS/script.js
@@ -0,0 +1,10 @@
+//BAsic Conditional Statement
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+//code performed if condition is true
+	console.log("You can ride the coaster!");
+}
+console.log("WHat comes after");
