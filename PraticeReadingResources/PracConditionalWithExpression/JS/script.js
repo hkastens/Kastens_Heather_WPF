7cb2858b5611349d66ba171241c5e67e01529b6e
hkastens <hkastens@fullsail.edu>
2013-11-13 17:04:35 -0600

    Prac Conditional with expression

---
+++ PraticeReadingResources/PracConditionalWithExpression/JS/script.js
@@ -0,0 +1,13 @@
+//Conditional Logic - With an Expression
+
+var kidHeight = 70;
+var minHeight = 48;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the child is over 48 inches in height
+
+if(kidHeight > minHeight){
+	console.log("You can ride!");
+}else{
+	console.log("Sorry kid");
+}
