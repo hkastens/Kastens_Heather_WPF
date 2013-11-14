2e8bfa960ec0e5805f09b632bd5d206c412e989c
hkastens <hkastens@fullsail.edu>
2013-11-13 18:27:39 -0600

    Praticing using || and ===

--- PraticeReadingResources/PraticeLogicOperators/JS/script.js
+++ PraticeReadingResources/PraticeLogicOperators/JS/script.js
@@ -1,13 +1,18 @@
 //Conditional Logic - Logical Operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var paycheck = 200;
-
+//var paycheck = 200;
+var wonLottery = true;
 //if price of the phone is less our budget and our paycheck is over 300
-if(iPhonePrice < budget && paycheck > 300){
+//if(iPhonePrice < budget && paycheck > 300){
+	//console.log("We can buy the phone");
+//}else{
+	//console.log("No phone for you");
+//}
+
+if(iPhonePrice < budget || wonLottery ===true){
 	console.log("We can buy the phone");
 }else{
 	console.log("No phone for you");
 }
-
