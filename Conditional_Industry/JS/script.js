978534efaa1e709bc9e32f5740a3737ad0a73157
hkastens <hkastens@fullsail.edu>
2013-11-13 20:05:51 -0600

    had to add ) and ; at the end of prompt two

--- Conditional_Industry/JS/script.js
+++ Conditional_Industry/JS/script.js
@@ -5,5 +5,5 @@
 var chicsOnHand = prompt ("How many chickens are currently in the warmer?"); 
 var supposedAmount = prompt ("How many chickens are supposed to be in the warmer at this time?");
 
-supposedAmount = (chicsOnHand >=) ? "You do not need to make more chicken" : "You need to make enough chicken so that you have the right amount on hand"
+supposedAmount = (chicsOnHand >=) ? "You do not need to make more chicken" : "You need to make enough chicken so that you have the right amount on hand");
 alert(supposedAmount);
