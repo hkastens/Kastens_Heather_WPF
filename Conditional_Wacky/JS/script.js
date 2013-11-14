98c82eb497c436003ef17a6eaed628c211e4f6be
hkastens <hkastens@fullsail.edu>
2013-11-13 20:57:51 -0600

    first if created

--- Conditional_Wacky/JS/script.js
+++ Conditional_Wacky/JS/script.js
@@ -6,3 +6,7 @@ var age = prompt ("Age of child?");
 var minAgeCat = prompt ("How old does your kid needs to be to have a cat?");
 var minAgeDog = prompt ("How old does your kid need to be to have a dog?");
 var minAgeFish = prompt ("How old does your kid need to be to have a fish?");
+
+if (age <= minAgeCat){
+	alert("Sorry you can't have a cat");
+}
