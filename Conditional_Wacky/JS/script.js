9f513a70c616443c0ccf7293fc0fb8a18514506b
hkastens <hkastens@fullsail.edu>
2013-11-13 21:19:51 -0600

    Changed var 1 and prompt 1

--- Conditional_Wacky/JS/script.js
+++ Conditional_Wacky/JS/script.js
@@ -2,7 +2,8 @@
 //November 13, 2013
 //Conditionals_Wacky
 
+var costOfDog = prompt ("How much will you need to buy the dog and all the necessities?");
 var moneyOnHand = prompt ("How much money you have on hand?");
-var moneyInSavings = prompt ("How much money do you have in savings?");
 var moneyYouExpectToMake= ("How much money do you expect to make between now and when you want to buy the dog?");
+
 
