e1d1b2143e7161317b48451c370b63e68de0dd3c
hkastens <hkastens@fullsail.edu>
2013-11-13 23:08:28 -0600

    Finally figured out how to add the vaules inputted in prompt instead of tacking the numbers on to each other

--- Conditional_Wacky/JS/script.js
+++ Conditional_Wacky/JS/script.js
@@ -4,15 +4,16 @@
 
 var costOfDog = prompt ("How much will you need to buy the dog and all the necessities?");
 var moneyOnHand = prompt ("How much money you have on hand?");
-var moneyYouExpectToMake= prompt ("How much money do you expect to make between now and when you want to buy the dog?");
+var moneyYouExpectToMake = prompt ("How much money do you expect to make between now and when you want to buy the dog?");
+moneyOnHand = Number (moneyOnHand);
+moneyYouExpectToMake = Number (moneyYouExpectToMake);
+var total = moneyOnHand + moneyYouExpectToMake; 
+alert("Your total amount will be " + total);
 
-
-var total = moneyOnHand + moneyYouExpectToMake;
-
-if (moneyOnHand >= costOfDog){
+/*if (moneyOnHand >= costOfDog){
 	alert("You can get the dog!");
 }else if (total >=){
 	alert("You can get the dog, but only after you bring home the amount you were expecting to bring home.");
 }else{
 	alert("Sorry you will not have enough money");
-}
+}*/
