e61e2f4839560c3d9276bb0428ac95c57dce4d9b
hkastens <hkastens@fullsail.edu>
2013-11-13 19:38:26 -0600

    Comment for what the console will say if his score is high enough

--- Conditional_Personal/JS/script.js
+++ Conditional_Personal/JS/script.js
@@ -3,7 +3,7 @@
 var sonsTestScore = 80; //The score he received on his test
 
 if(sonsTestScore >= 70){ //Equation to use to see if son can go to friends this weekend. If his score is better then or equal to 70 he can go
-	console.log("He can go to his friends this weekend!");
+	console.log("He can go to his friends this weekend!"); //What will be printed to console if his score is high enough
 }else{
 	console.log("He needs to study this weekend");
 }
