6960a840eb5e67aab146d17609240c2b4632a5a1
hkastens <hkastens@fullsail.edu>
2013-11-13 23:33:14 -0600

    Changed the the final alert to be console instead

--- Conditional_Wacky/JS/script.js
+++ Conditional_Wacky/JS/script.js
@@ -11,9 +11,9 @@ var total = moneyOnHand + moneyYouExpectToMake; //The nuber values of each are b
 alert("Your total amount will be " + total); //This is an alret to let user know much much money intotal they will have
 
 if(moneyOnHand >= costOfDog){ //if the moneyOnHnad is greater than or equal to the costOfTheDog
-	alert("You can get the dog!"); //This alret will pop up if the above statement is true
+	console.log("You can get the dog!");
 }else if (total >= costOfDog){ //if the total is greater than or equal to the costOfDog
-	alert("You can get the dog, but only after you bring home the amount you were expecting to bring home."); //this is the alert that will pop up if the above statement is true
+	console.log("You can get the dog, but only after you bring home the amount you were expecting to make");
 }else{ //this is saying if neither one is true then
-	alert("Sorry you will not have enough money"); //This is the alert that will pop up if you will not have enough money to buy the dog
+	console.log("Sorry you will not have enough money");
 }
