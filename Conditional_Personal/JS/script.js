29709704f5ed2cdc6f3dc4e768a7cb15d4d535e3
hkastens <hkastens@fullsail.edu>
2013-11-13 19:37:19 -0600

    Comment added to "if" in personal

--- Conditional_Personal/JS/script.js
+++ Conditional_Personal/JS/script.js
@@ -2,7 +2,7 @@
 
 var sonsTestScore = 80; //The score he received on his test
 
-if(sonsTestScore >= 70){
+if(sonsTestScore >= 70){ //Equation to use to see if son can go to friends this weekend. If his score is better then or equal to 70 he can go
 	console.log("He can go to his friends this weekend!");
 }else{
 	console.log("He needs to study this weekend");
