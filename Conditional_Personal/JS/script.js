28bf07df437a071a234a42550b6f37d18911d37e
hkastens <hkastens@fullsail.edu>
2013-11-13 23:43:15 -0600

    changed alert to console

--- Conditional_Personal/JS/script.js
+++ Conditional_Personal/JS/script.js
@@ -9,5 +9,5 @@ var lowestScore = prompt ("What is the lowest score they can get, in order to go
 if(sonsTestScore >= lowestScore){ //Equation to use to see if son can go to friends this weekend. If his score is better then or equal to 70 he can go
 	alert("He can go to his friends this weekend!"); //What will be printed to console if his score is high enough
 }else{ //Used the else to show that there is an alternative if score is not high enough
-	alert("He needs to study this weekend");// what the console will show if his score is not high enough
+	console.log("He needs to study this weekend");// what the console will show if his score is not high enough
 }
