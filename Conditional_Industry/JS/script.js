093d145becb4d814c4393acb865797d4cc48cf54
hkastens <hkastens@fullsail.edu>
2013-11-13 20:02:58 -0600

    First half of ternary created

--- Conditional_Industry/JS/script.js
+++ Conditional_Industry/JS/script.js
@@ -4,3 +4,5 @@
 
 var chicsOnHand = prompt ("How many chickens are currently in the warmer?"); 
 var supposedAmount = prompt ("How many chickens are supposed to be in the warmer at this time?");
+
+supposedAmount = (chicsOnHand >=) ? "You do not need to make more chicken"
