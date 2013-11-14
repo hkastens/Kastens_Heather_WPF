163db6f9fdcacaaa8933fb59f70ddd9e89c974c9
hkastens <hkastens@fullsail.edu>
2013-11-13 19:36:00 -0600

    Comment added to var on personal

--- Conditional_Personal/JS/script.js
+++ Conditional_Personal/JS/script.js
@@ -1,6 +1,6 @@
 //Conditional_Personal
 
-var sonsTestScore = 80;
+var sonsTestScore = 80; //The score he received on his test
 
 if(sonsTestScore >= 70){
 	console.log("He can go to his friends this weekend!");
