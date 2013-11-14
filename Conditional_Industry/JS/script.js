0ba5cf6c3ccec8b4cab28f2b3db49cd7305be34e
hkastens <hkastens@fullsail.edu>
2013-11-13 23:41:02 -0600

    Comments added

--- Conditional_Industry/JS/script.js
+++ Conditional_Industry/JS/script.js
@@ -2,9 +2,9 @@
 //November 13, 2013
 //Conditionals_Industry
 
-var chicsOnHand = prompt ("How many chickens are currently in the warmer?"); 
-var supposedAmount = prompt ("How many chickens are supposed to be in the warmer at this time?");
-var howManyChicToMake;
+var chicsOnHand = prompt ("How many chickens are currently in the warmer?"); //This is the number of chickens that are already made
+var supposedAmount = prompt ("How many chickens are supposed to be in the warmer at this time?");// This is the amount of chickens that are supposed to be on done
+var doIMakeChics; //This is the question we are solving for 
 
-howManyChicToMake = (chicsOnHand >= supposedAmount) ? "You do not need to make more chicken" : "You need to make enough chicken so that you have the right amount on hand";
-console.log(howManyChicToMake);
+doIMakeChics = (chicsOnHand >= supposedAmount) ? "You do not need to make more chicken" : "You need to make enough chicken so that you have the right amount on hand";//Used Ternary style code to determine if we needed more chickens 
+console.log(howManyChicToMake);// The answer will be printed on the console
