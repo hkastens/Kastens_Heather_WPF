33829ddb7ad05be3b40c65f2e4056365eb2382e8
hkastens <hkastens@fullsail.edu>
2013-11-13 19:47:22 -0600

    Had to go back and make prompt for var one

--- Conditional_Personal/JS/script.js
+++ Conditional_Personal/JS/script.js
@@ -1,7 +1,9 @@
 //Conditional_Personal
 
-var sonsTestScore = 80; //The score he received on his test
 
+
+var sonsTestScore = prompt ("What was his/her test score?"); //The score he received on his test
+var lowestScore = prompt
 if(sonsTestScore >= 70){ //Equation to use to see if son can go to friends this weekend. If his score is better then or equal to 70 he can go
 	console.log("He can go to his friends this weekend!"); //What will be printed to console if his score is high enough
 }else{ //Used the else to show that there is an alternative if score is not high enough
