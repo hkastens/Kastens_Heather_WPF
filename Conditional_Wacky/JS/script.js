ca58339daa750215e095e460d9a49c64fb7b75bd
hkastens <hkastens@fullsail.edu>
2013-11-13 21:12:35 -0600

    Deleted everything. Wasnt working out right.

--- Conditional_Wacky/JS/script.js
+++ Conditional_Wacky/JS/script.js
@@ -2,15 +2,4 @@
 //November 13, 2013
 //Conditionals_Wacky
 
-var age = prompt ("Age of child?");
-var minAgeCat = prompt ("How old does your kid needs to be to have a cat?");
-var minAgeDog = prompt ("How old does your kid need to be to have a dog?");
 
-
-if (age <= minAgeCat){
-	alert("Sorry you can't have a cat");
-}else if (age <= minAgeDog){
-	alert("Sorry you can't have a fish");
-}else{
-	alert("Sorry, you can have a fish");
-} 
