8b6f382349912390300dfd14ce42c552f83a2c67
hkastens <hkastens@fullsail.edu>
2013-11-20 14:32:43 -0600

    Practice loops work along

---
+++ PraticeLoops/Loops/JS/script.js
@@ -0,0 +1,5 @@
+//For Loop
+
+for (var i = 10; i > 0; i--){
+	console.log ( i + " Kegs on the wall")
+}
---
+++ PraticeLoops/WhileLoops/JS/script.js
@@ -0,0 +1,20 @@
+//While Loops
+
+var b = 10;// sets up index
+
+while (b > 0){//checks the condition
+	console.log( b + " Kegs on the wall");
+	b--;//incriments or decraments the index
+
+}
+
+
+console.log("-----Do WHile Loops-------");
+
+var c = 10;
+
+do{
+	console.log ( c + " Kegs on the wall");
+	c--;
+}
+while(c > 0);
