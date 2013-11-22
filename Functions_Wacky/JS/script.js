01a91723e7522d36f20444a0e6f91b9f6b600dbc
hkastens <hkastens@fullsail.edu>
2013-11-21 23:30:54 -0600

    red, blue and green converted to actuall numbers to be summed together

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -6,4 +6,8 @@ var blue = prompt ("How many blue balloons would you like?");
 var red = prompt ("How many red balloons would you like?");
 var green = prompt ("How many green balloons would you like?");{
 	var total = function (blue, red, green)
+	
+	blue = Number (blue);
+	red = Number (Red);
+	green = Number (green);
 }
