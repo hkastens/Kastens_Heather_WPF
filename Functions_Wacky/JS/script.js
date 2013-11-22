5a4dadab46e0e8b1d04b0bbd6d869708f1c2aa00
hkastens <hkastens@fullsail.edu>
2013-11-21 23:26:22 -0600

    Third prompt created

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -4,4 +4,4 @@
 
 var blue = prompt ("How many blue balloons would you like?");
 var red = prompt ("How many red balloons would you like?");
-
+var green = prompt ("How many green balloons would you like?");
