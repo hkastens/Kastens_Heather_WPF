8011fc0a4112fc1ad09649d1cac5917639d5aaa9
hkastens <hkastens@fullsail.edu>
2013-11-21 22:57:02 -0600

    console created

--- Functions_Industry/JS/script.js
+++ Functions_Industry/JS/script.js
@@ -2,10 +2,11 @@
 //Functions Industry
 //November 21, 2013
 
-var chickenBoxs = alert ("Any box of chicken that is 25 pieces and up are $2.00 off");
+var chickenBoxs = alert ("Any box of chicken that is 25 pieces and up are $2.00 off \n normal price of chicken is a dollar a piece of chicken");
 
 var boxSize = prompt ("How many pieces of chicken would you like?");
 	if (boxSize > 8){
 		
 		boxSize = Number (boxSize);
+		console.log("The sale price of your chicken box is " + TheboxSize - 2 + " dollars");
 	}
