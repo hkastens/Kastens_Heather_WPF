1c82b5d7ccd6adaa00011f3898a33aa777077890
hkastens <hkastens@fullsail.edu>
2013-11-21 23:21:50 -0600

    Added and = sign with the > sign

--- Functions_Industry/JS/script.js
+++ Functions_Industry/JS/script.js
@@ -5,7 +5,7 @@
 var chickenBoxs = alert ("Any box of chicken that is 25 pieces and up are $2.00 off \n normal price of chicken is a dollar a piece of chicken");//Alert telling about the sale
 
 var boxSize = prompt ("How many pieces of chicken would you like?");//The amount of chicken the customer would like
-	if (boxSize > 8){//if more then 8 pieces are wanted 
+	if (boxSize >= 8){//if more then 8 pieces are wanted 
 		
 		boxSize = Number (boxSize);//setting up equation to use inputted number to be actual number 
 		console.log("The sale price of your chicken box is " + (boxSize - 2) + " dollars");//The sale price pf chicken is printed to console
