0e7e6dbe2396e92140f31fe7bb8a76883da4bc94
hkastens <hkastens@fullsail.edu>
2013-11-21 21:09:21 -0600

    Calculation made

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -4,5 +4,8 @@
 
 var numOfAs = function (A, moneyEarned) {
 	var A = prompt ("How many A's did the child receive?");
-	var moneyEarned
+	var moneyEarned ("How much money is each A worth?");
+	
+	var totalMoney = A * moneyEarned;
+	con
 }
