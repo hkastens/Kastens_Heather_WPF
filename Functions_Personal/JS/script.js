8491bd950d3cc6500065edf34ab810fd81782d73
hkastens <hkastens@fullsail.edu>
2013-11-21 22:14:07 -0600

    Been trying to get my prompts to work with the functions. Not having very good luck.

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -2,15 +2,18 @@
 //Functions_Personal
 //November 21, 2013
 
-var numOfAs = function (A, moneyEarned) {
-	var A = prompt ("How many A's did the child receive?");
-	var moneyEarned ("How much money is each A worth?");
+
+
+var A = prompt ("How many A's did the child receive?");
+var moneyEarned = prompt ("How much money is each A worth?");{
+   
+    var moneyPerA function(A, moneyEarned);
+	A = Number (A);
+    moneyEarned = Number (moneyEarned);
+	var totalMoney = (A * moneyEarned);
+	console.log(totalMoney);
+}	
 	
-	var totalMoney = A * moneyEarned;
-	return totalMoney;
-}
 
-var A = Number (A);
-var moneyEarned = Number (moneyEarned);
-var total = moneyEarned;
 
+    
