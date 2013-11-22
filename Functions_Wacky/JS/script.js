d51faffcfe443ecd57f3e14e7ff78ed0a73ee85a
hkastens <hkastens@fullsail.edu>
2013-11-22 00:08:07 -0600

    Deleted everything to start over

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -2,15 +2,5 @@
 //Functions Wacky
 //November 21, 2013
 
-var blue = prompt ("How many blue balloons would you like?");
-var red = prompt ("How many red balloons would you like?");
-var green = prompt ("How many green balloons would you like?");{
-	var total = function (blue, red, green)
-	
-	blue = Number (blue);
-	red = Number (red);
-	green = Number (green);
-	
-	var totalBalloons = (blue + red + green);
-	console.log("The total number of balloons that the customer would like is " + totalBalloons);
-}
+
+
