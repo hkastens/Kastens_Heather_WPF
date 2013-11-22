248b937180a129e030de999ab00f83df3d6f7922
hkastens <hkastens@fullsail.edu>
2013-11-22 00:18:53 -0600

    Fixed error

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -3,4 +3,10 @@
 //November 21, 2013
 
 
+var number= prompt ("What number are you wanting to calculate to the third power?");{
+	var thirdPower = function (number, number, numer)
+	number = Number (number);
 
+	var total = (number * number * number);
+	console.log(total);
+}
