ae5dc82cdb676bc1123e6a32c8e57fa82ee6bcc3
hkastens <hkastens@fullsail.edu>
2013-11-21 22:29:05 -0600

    After an hour finally got everything to work.

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -4,14 +4,14 @@
 
 
 
-var A = prompt ("How many A's did the child receive?");
-var moneyEarned = prompt ("How much money is each A worth?");{
-    var moneyPerA = function (A, moneyEarned) 
+var A = prompt ("How many A's did the child receive?");//first prompt
+var moneyEarned = prompt ("How much money is each A worth?");{//second prompt
+    var moneyPerA = function (A, moneyEarned) //difining
     
-	A = Number (A);
-    moneyEarned = Number (moneyEarned);
-	var totalMoney = (A * moneyEarned);
-	console.log(totalMoney);
+	A = Number (A);//truning the var A into the actual number 
+    moneyEarned = Number (moneyEarned);//turning var moneyEarned into actual number to be add
+	var totalMoney = (A * moneyEarned);//taking the amount of A's earned and muiltipling them by the dollar amount earned per A
+	console.log("Your child earned a total of " + totalMoney + " dollars");
 }	
 	
 
