1f88144beae5af001075f1abe68d127334dd8505
hkastens <hkastens@fullsail.edu>
2013-11-21 22:20:10 -0600

    Still trying to rearange so this works. only wants to work when I take the function part out.

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -6,8 +6,8 @@
 
 var A = prompt ("How many A's did the child receive?");
 var moneyEarned = prompt ("How much money is each A worth?");{
-   
-    var moneyPerA function(A, moneyEarned);
+    var moneyPerA = function (A, moneyEarned); 
+    
 	A = Number (A);
     moneyEarned = Number (moneyEarned);
 	var totalMoney = (A * moneyEarned);
