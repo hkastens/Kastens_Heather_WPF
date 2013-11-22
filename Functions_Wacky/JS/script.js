eebd9c3dcfebd619521f3c522b950b09f6ce37bb
hkastens <hkastens@fullsail.edu>
2013-11-22 00:21:12 -0600

    Comments added

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -3,10 +3,10 @@
 //November 21, 2013
 
 
-var number= prompt ("What number are you wanting to calculate to the third power?");{
-	var thirdPower = function (number, number, numer)
-	number = Number (number);
+var number= prompt ("What number are you wanting to calculate to the third power?");{//The number that you are sloving for
+	var thirdPower = function (number, number, numer)//the function
+	number = Number (number);//turning the number into an actual number
 
-	var total = (number * number * number);
-	console.log(total);
+	var total = (number * number * number);//taking the number inputed find the third power of it
+	console.log("Your answer is " + total);//total printed to console
 }
